ad89bba922c2f3de1389c887b3919658c614e5f9
Debaprasad <debaprasadmaiti2000@gmail.com>
2025-12-26 22:49:39 +0530

    Update API base URL for contact API

--- frontend/src/api/contact.js
+++ frontend/src/api/contact.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://6mqmf6l1-5000.inc1.devtunnels.ms/api",
+  baseURL: "https://travel-maniya.onrender.com/api",
 });
 
 export const getMessages = () =>
